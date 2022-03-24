7d790799f70697cff0f89c49a49ccfcb416121fd
andrelobo <loboandre@hotmail.com>
2022-03-24 18:22:30 -0400

    fix: css fix

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,7 @@ import Image from 'next/image'
 import duascabecasigor from '../public/duascabecasigor.png'
 import profile from '../public/profile.png'
 import styles from '../styles/Home.module.css'
-import { Icon } from '@iconify/react';
-
-
+import { Icon } from '@iconify/react'
 
 export default function Home() {
   return (
@@ -14,47 +12,44 @@ export default function Home() {
         <title>IGOR - R.U.A</title>
         <meta name="description" content="Grafite de Manaus" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>      
+      </Head>
 
       <main className={styles.main}>
 
-                 
-                  <Image className={styles.duascabecas} src= {duascabecasigor} alt="Banner_Igor" />
+     
+        <Image
+          
+          src={duascabecasigor}
+          alt="Banner_Igor"
+        />
+      
+       <div className={styles.prof} >
+        <Image className={styles.profile} src={profile} alt="profile" />
 
+        </div>
 
-                  <Image className={styles.profile} src= {profile} alt="profile" />
-                
+        <h1 className={styles.barrio}>IGOR</h1>
 
-                  
-                    <h1 className={styles.barrio}>IGOR</h1>
+        <ul>
+          <li>
+            <Icon icon="la:instagram" />
+          </li>
+          <li>
+            <Icon icon="la:whatsapp" />
+          </li>
+          <li>
+            <Icon icon="la:youtube" />
+          </li>
+        </ul>
 
-                    
-                    <ul>
-                    <li><Icon icon="la:instagram" /></li>
-                    <li><Icon icon="la:whatsapp" /></li>
-                    <li><Icon icon="la:youtube" /></li>        
-                  
-                  
-                  </ul> 
-         
-       
+        
       </main>
 
-    
-
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-          <span className={styles.barrio}>
+               <span className={styles.barrio}>
             <h2>R.U.A</h2>
-
-
           </span>
-        </a>
+      
       </footer>
     </div>
   )
